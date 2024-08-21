f67c2d2f121dd80f811e1fe714b6e83a7ff78c73
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-08-21 16:41:01 -0300

    Component Wave

---
+++ src/components/ondas/Wave.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Wave() {
+    return (
+        <div>
+            <div className='wave'>
+            <svg viewBox="0 0 904 155" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="#372B2B" d="M 0 87 C 216 87 144 85 360 85 L 360 85 L 360 0 L 0 0 Z" stroke-width="0"></path> <path fill="#372B2B" d="M 359 85 C 686 85 577 131 904 131 L 904 131 L 904 0 L 359 0 Z" stroke-width="0"></path> </svg>
+            </div>
+        </div>
+    )
+}
+
+export default Wave
